f5d0d97a1d7044e7d7704baa99c7777bf33be342
Janine Hempy <jhempy@gmail.com>
2018-11-09 08:48:32 -0500

    Resets everything for classroom demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,16 +84,8 @@
                     Laravel
                 </div>
 
-<p>Hey, this is Janine typing!</p>
-
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/things">Things</a>
                 </div>
             </div>
         </div>
